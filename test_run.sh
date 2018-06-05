59b5aeb0da041a6aa7e61aadf4300fe3c55c28fe
Jonny Shipton <jonny@myrtlesoftware.com>
2018-06-05 10:34:37 +0100

    Add demo from file to file

---
+++ test_run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python Scripts/file_demo.py --model Example_Models/segnet_model_driving_webdemo.prototxt --weights Example_Models/segnet_weights_driving_webdemo.caffemodel --colours Scripts/camvid12.png --input CamVid/test/0001TP_008550.png
